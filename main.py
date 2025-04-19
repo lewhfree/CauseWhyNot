1a38dbc74bd9003ce2c321e10e9d0ce5ab647902
Lewis <lewhfree@gmail.com>
2025-04-19 14:58:54 -0600

    Final update, should be good

--- main.py
+++ main.py
@@ -5,21 +5,19 @@ pattern = r'^\d+\.\d{2}$'
 
 numArgs = len(sys.argv)
 
-if numArgs == 1:
-    print("Expected a filename argument")
+if numArgs < 3:
+    print("Expected 2 filename arguments - one input, one output")
     sys.exit()
+
+if numArgs >= 4:
+    #Dump rest of the bin after the stub
+    print("Dumping binary after stub to", sys.argv[3])
 
 fileName = sys.argv[1]
 
 print("Opening:", fileName)
 
 with open(fileName, "rb") as file:
-    #file.seek(1241)
-    #byte = file.read(4)
-    #byte = byte.decode('ascii')
-    #print(str(byte))
-    
-
     for i in range(5):
         knownVOffsets = [1241, 1273, 1257, 1225, 1289]
         file.seek(knownVOffsets[i])
@@ -33,13 +31,36 @@ with open(fileName, "rb") as file:
                 print("CauseWay not found or unsupported version")
                 sys.exit()
     
-    #Valid CauseWay executable, I think it is just the first 0xb680 but 
-
-    repeatableString = "d138d1e8d1e88cc303c38ec05b582bf8"
-    offset = file.read().find(bytes.fromhex(repeatableString))
+    file.seek(0)
+    data = file.read()
+    repeatableString = "83e00f03f8e9eafec300000000000000"
+    offset = data.find(bytes.fromhex(repeatableString))
 
     if offset == -1:
-        print("string not found")
+        print("CauseWay magic bytes not found.")
         sys.exit()
     else:
-        print("OFFSET FOUND AT", offset)
+        print("CauseWay magic bytes at:", offset)
+                    #This is here because the length is 32 when its 16 bytes, you could also do bytes.fromhex
+    eos = offset + int(len(repeatableString) / 2)
+    
+    outputFile = sys.argv[2]
+    with open(outputFile, "wb") as out:
+        print("Opening", outputFile,"to write.")
+        dataToEos = data[:eos]
+        out.write(dataToEos)
+        print("Wrote", len(dataToEos), "bytes to file")
+    print("Closed", outputFile)
+    
+    if numArgs >= 4:
+        outputFile = sys.argv[3]
+        with open(outputFile, "wb") as out:
+            print("Opening", outputFile, "to write.")
+            dataFromEos = data[eos:]
+            out.write(dataFromEos)
+            print("Wrote",len(dataFromEos), "bytes to file")
+        print("Closed", outputFile)
+
+print("Closed", sys.argv[1])
+
+
